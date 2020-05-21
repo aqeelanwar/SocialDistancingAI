003d868b67477e48836af999a2841ee580acaa22
Karishnu Poddar <karishnu@gmail.com>
2020-05-22 01:43:20 +0530

    Use saved model instead of frozen graph (depreciated), Use get_file utility to handle model download, Use new create_category_index_from_labelmap util method

--- backbone.py
+++ backbone.py
@@ -27,31 +27,18 @@ def set_model(model_name, label_name):
 
     num_classes = 90
 
-    # Download Model if it has not been downloaded yet
-    if model_found == 0:
-        opener = urllib.request.URLopener()
-        opener.retrieve(download_base + model_file, model_file)
-        tar_file = tarfile.open(model_file)
-        for file in tar_file.getmembers():
-            file_name = os.path.basename(file.name)
-            if "frozen_inference_graph.pb" in file_name:
-                tar_file.extract(file, os.getcwd())
+    base_url = 'http://download.tensorflow.org/models/object_detection/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(
+        fname=model_name,
+        origin=base_url + model_file,
+        untar=True)
 
-    # Load a (frozen) Tensorflow model into memory.
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(path_to_ckpt, "rb") as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name="")
+    model_dir = os.path.join(model_dir, "saved_model")
 
-    # Loading label map
-    # Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane. Here I 		use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-    label_map = label_map_util.load_labelmap(path_to_labels)
-    categories = label_map_util.convert_label_map_to_categories(
-        label_map, max_num_classes=num_classes, use_display_name=True
-    )
-    category_index = label_map_util.create_category_index(categories)
+    model = tf.saved_model.load(str(model_dir))
+    model = model.signatures['serving_default']
 
-    return detection_graph, category_index
+    category_index = label_map_util.create_category_index_from_labelmap(path_to_labels, use_display_name=True)
+
+    return model, category_index
